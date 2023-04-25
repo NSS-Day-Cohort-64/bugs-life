be1f4139ef49726e92d7e3f78fb0b7c8bf7da846
Tiana Robinson <trobinson1097@gmail.com>
2023-04-25 12:51:39 -0700

    created a litst of Terrariums

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 import { BugHTMLList } from "./Bugs.js"
+import { TerrariumHtmlList } from "./Terrariums.js"
 
 const container = document.querySelector("#container")
 
 // Compose ALL of the HTML and update the DOM
-container.innerHTML = BugHTMLList()
+container.innerHTML = BugHTMLList() + TerrariumHtmlList()
+
 
 
