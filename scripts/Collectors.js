f4fba60a82ee330debc70c2c02599d7385ebb9fd
Greg Korte <gregmkorte@gmail.com>
2023-04-26 09:42:33 -0500

    Making changes to the naming convention for the HTML string.

--- scripts/Collectors.js
+++ scripts/Collectors.js
@@ -7,18 +7,18 @@ const collectorArray = getCollectorsObjects()
 // define a function that will create the HTML sting of collectors
 export const CollectorHTMLList = () => {
    // - Build an empty HTML string
-    let htmlString = `<h2>Collector List</h2>
+    let HTMLString = `<h2>Collector List</h2>
     <ul>`
     // iterate array of collectors
     for (const collector of collectorArray){
-        // - add the collector to html string
+        // - add the collector to HTML string
             // display a list of all collectors
             // display the name
-        htmlString += `<li>${collector.name}</li>`
+        HTMLString += `<li>${collector.name}</li>`
     }
-   // - close our html string
-    htmlString += `</ul>`
+   // - close our HTML string
+    HTMLString += `</ul>`
 
-    // - return html sting that has been modified
-    return htmlString
+    // - return HTML sting that has been modified
+    return HTMLString
 }
